a4ab81ef4f48d531de24b852be83104fa59ef7db
Baivab <baivabdm@gmail.com>
2025-02-20 01:52:58 +0530

    Create callbacks to generate output and modify new plan and reset button functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc, callback, Output, Input, ALL, State
+from dash import Dash, html, dcc, callback, Output, Input, ALL, State, dash_table
 from dash.exceptions import PreventUpdate
 import dash_bootstrap_components as dbc
 import frontpage as fp
@@ -9,17 +9,23 @@ import threading as th
 import numpy as np
 
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.title = "Calorie Calculator and Optimizer"
 app.layout = [
-    dcc.Store(id="n_clicks_vector", data=[]),
+    dcc.Store(id="reqd_bmr", data=[]),
+    dcc.Store(id="diet-cal", data=[]),
     *fp.frontPageItems]
 
 
 @callback(
     Output("planner-form-col-row-1", "children", allow_duplicate=True),
+    Output("planner-output-col-row-1", "children", allow_duplicate=True),
     Output("new-plan", "n_clicks"),
     Output("reset", "n_clicks"),
     Output("planner-form-col-row-2", "children", allow_duplicate=True),
     Output("planner-form-col-row-3", "children"),
+    Output("planner-output-col-row-2", "children", allow_duplicate=True),
+    Output("planner-output-col-row-3", "children", allow_duplicate=True),
+    Output("planner-output-col-row-4", "children", allow_duplicate=True),
     Output("submit", "style"),
     Input("new-plan", "n_clicks"),
     Input("reset", "n_clicks"),
@@ -27,10 +33,10 @@ app.layout = [
 )
 def new_plan(n_clicks_plan,n_clicks_reset,):
     if n_clicks_plan == 1:
-        return [[pl.create_weight_input_widget(), pl.create_form_row()], 2, 0, [], pl.create_add_item_button(), {}]
+        return [pl.create_weight_input_widget()+[pl.create_form_row()], [], 2, 0, [], pl.create_add_item_button(), [], [], [], {}]
 
     elif n_clicks_reset == 1:
-        return [[], 0, 0, [], [], dict(display='none')]
+        return [[], [], 0, 0, [], [], [], [], [], dict(display='none')]
     else:
         raise PreventUpdate
 
@@ -62,12 +68,11 @@ def on_row_delete_click(row1_children, row2_children, n_clicks_dynamic):
 
     n_clicks_vector = np.array(n_clicks_dynamic)
     clickedButton = n_clicks_vector.argmax()+1
-    print(clickedButton)
-    if clickedButton == 1 and len(row1_children) == 2 and n_clicks_vector.max() != 0:
+    if clickedButton == 1 and len(row1_children) == 7 and n_clicks_vector.max() != 0:
         row1_children.pop()
-    elif len(row1_children) == 1 and n_clicks_vector.max() != 0:
+    elif len(row1_children) == 6 and n_clicks_vector.max() != 0:
         row2_children.pop(clickedButton-1)
-    elif len(row1_children) == 2 and n_clicks_vector.max() != 0:
+    elif len(row1_children) == 7 and n_clicks_vector.max() != 0:
         row2_children.pop(clickedButton-2)
     else:
         raise PreventUpdate
@@ -76,9 +81,71 @@ def on_row_delete_click(row1_children, row2_children, n_clicks_dynamic):
     return [row1_children, row2_children, zero_vector.astype(int).tolist()]
 
 
+@callback(
+    Output("planner-output-col-row-1", "children"),
+    Output("reqd_bmr", "data"),
+    Input("weight-input", "value"),
+    Input("age-input", "value"),
+    Input("height-input", "value"),
+    Input("gender-input", "value")
+)
+def calculate_requirements(weight, age, height, gender):
+    reqd_row, bmr = pl.calculate_requirements(weight, age, height, gender)
+
+    return [reqd_row, bmr]
+
+
+@callback(
+    Output("planner-output-col-row-2", "children"),
+    Output("planner-output-col-row-4", "children"),
+    Output("diet-cal", "data"),
+    Input({"type": "form-dropdown", "index": ALL}, "value"),
+    Input({"type": "form-input", "index": ALL}, "value"),
+    prevent_initial_call=True
+)
+def calculate_kpis(dropdowns, inputs):
+
+    contents = {}
+
+    for drop, inp in zip(dropdowns, inputs):
+        if drop is not None and inp is not None:
+            if contents.get(drop, "not found") == "not found":
+                contents[drop] = [inp]
+            else:
+                contents[drop].append(inp)
+
+    if len(contents)==0:
+        return [[], 0]
+
+    plan_Row, calories, nutrients_df = pl.calculate_diet_kpis(contents)
+
+    nutrients_data_table = dash_table.DataTable(nutrients_df.to_dict("records"), [{"name": i, "id": i} for i in nutrients_df.columns], id="nutrients-table")
+
+    return [plan_Row, nutrients_data_table, calories]
+
+
+@callback(
+    Output("planner-output-col-row-3", "children"),
+    Input("reqd_bmr", "data"),
+    Input("diet-cal", "data")
+)
+def get_calorie_difference(bmr, planned_cal):
+    diff = round(planned_cal-bmr,2)
+
+    if diff < 0:
+        diff_string = dbc.Row([html.H5("Calorie Deficit:", id="deficit-heading"), html.H6(f"{diff} Cal", id="deficit-value")])
+    elif diff > 0:
+        diff_string = dbc.Row([html.H5("Calorie Surplus:", id="surplus-heading"), html.H6(f"{diff} Cal", id="surplus-value")])
+    else:
+        diff_string = dbc.Row([html.H5("Calorie Balance:", id="balance-heading"), html.H6(f"{diff} Cal", id="balance-value")])
+
+    return diff_string
+
+
+
 if __name__ == "__main__":
     port=33115
-    debugMode = False
+    debugMode = True
     open_browser = Timer(3, wb.open, [f"http://127.0.0.1:{port}"]).run
     openBrowserThread = th.Thread(target=open_browser)
     openBrowserThread.start()

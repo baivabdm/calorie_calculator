9c91a24ce8e258b7e024e433a4152b9572da2f95
Baivab <baivabdm@gmail.com>
2025-02-22 21:53:48 +0530

    Add history tab frontend contents to tab layout

--- frontpage.py
+++ frontpage.py
@@ -1,6 +1,7 @@
 from dash import html
 import dash_bootstrap_components as dbc
 import planner
+import history
 
 frontPageItems = [
     html.Div(
@@ -15,8 +16,8 @@ frontPageItems = [
                     dbc.Tabs(
                              children=[
                                  dbc.Tab(label="Planner", id="tab-planner", children=planner.contents),
-                                 dbc.Tab(label="History", id="tab-history", children=[]),
-                                 dbc.Tab(label="DB Management", id="tab-db-management", children=[])
+                                 dbc.Tab(label="History", id="tab-history", children=history.contents),
+                                 dbc.Tab(label="DB Management", id="tab-db-management", children=[], disabled=True)
                              ],
                              id="tab-group", active_tab = "tab-0",
                              )
